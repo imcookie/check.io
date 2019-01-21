e67077129eae77d7bed82c53d44b9eec236d308b
un <hd.ilin@yandex.ru>
2019-01-21 14:08:45 +0300

    Most_Wanted_Letter solution

---
+++ The_Most_Wanted_Letter/Main.py
@@ -0,0 +1,22 @@
+import re
+from collections import Counter
+def checkio(text: str) -> str:
+    count = Counter(re.findall("[a-z]", text.lower())).most_common()
+    result = min(count, key=lambda x: (-x[1], x[0]))
+    return result[0]
+
+
+if __name__ == '__main__':
+    print("Example:")
+    print(checkio("Hello World!"))
+
+    # These "asserts" using only for self-checking and not necessary for auto-testing
+    assert checkio("Hello World!") == "l", "Hello test"
+    assert checkio("How do you do?") == "o", "O is most wanted"
+    assert checkio("One") == "e", "All letter only once."
+    assert checkio("Oops!") == "o", "Don't forget about lower case."
+    assert checkio("AAaooo!!!!") == "a", "Only letters."
+    assert checkio("abe") == "a", "The First."
+    print("Start the long test")
+    assert checkio("a" * 9000 + "b" * 1000) == "a", "Long."
+    print("The local tests are done.")
